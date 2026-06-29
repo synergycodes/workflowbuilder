82c684710898767948468dc82edb6690343247b9
Piotr Błaszczyk <105283001+piotrblaszczyk@users.noreply.github.com>
2026-06-29 11:23:38 +0200

    feat: keyboard zoom + watermark layering fix (#53)
    * feat: keyboard zoom + watermark layering fix
    
    * feat: keyboard zoom + watermark layering fix - add missing files

--- packages/sdk/src/hooks/use-command-handler-keyboard.ts
+++ packages/sdk/src/hooks/use-command-handler-keyboard.ts
@@ -6,10 +6,25 @@ import { useKeyPress } from './use-key-press';
 
 export function useCommandHandlerKeyboard(commandHandler: CommandHandler) {
   const a = useKeyPress('a', { withControlOrMeta: true });
+  const plus = useKeyPress('+', { withControlOrMeta: true });
+  const equal = useKeyPress('=', { withControlOrMeta: true });
+  const minus = useKeyPress('-', { withControlOrMeta: true });
 
   useEffect(() => {
     if (a) {
       commandHandler.selectAll();
     }
   }, [a]);
+
+  useEffect(() => {
+    if (plus || equal) {
+      commandHandler.zoomIn();
+    }
+  }, [plus, equal]);
+
+  useEffect(() => {
+    if (minus) {
+      commandHandler.zoomOut();
+    }
+  }, [minus]);
 }
