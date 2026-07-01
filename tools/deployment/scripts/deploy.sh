2396cddea358f97e7077ba59614bfe9ab9a532e0
Jakub Kubacki <jakub.kubacki@synergycodes.com>
2026-07-01 11:56:07 +0200

    Remove old deployment code

--- tools/deployment/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Deploy the AI Studio stack to Swarm, mirroring workflow-builder's
-# deploy.sh. The setup scripts exist only in the deployment CI image.
-set -eu
-
-[ -f /var/setup-az.sh ] && . /var/setup-az.sh
-[ -f /var/setup-ansible.sh ] && . /var/setup-ansible.sh
-
-ansible-playbook ./tools/deployment/ansible/deploy-application/main.yml
