db512e68dedeee02717f133397803d34b6c0f17f
Szymon Tondowski <szymon.tondowski@synergycodes.com>
2026-08-28 13:21:25 +0200

    fix(sdk): variable-select blur action fix

--- packages/sdk/src/features/variables/components/variable-select/variable-select.tsx
+++ packages/sdk/src/features/variables/components/variable-select/variable-select.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './variable-select.module.css';
@@ -17,30 +17,36 @@ type Props = VariableTextProps & {
 export function VariableSelect({ onChange, onBlur, endAdornment, ...props }: Props) {
   const { t } = useTranslation();
 
+  // We use ref because state mutates the onBlur which prop drill isn't updated in time
+  const inputValueRef = useRef<string>(props.value == null ? '' : String(props.value));
+
+  useEffect(() => {
+    inputValueRef.current = props.value == null ? '' : String(props.value);
+  }, [props.value]);
+
   const handleOnChange: VariableTextProps['onChange'] = useCallback(
     (value) => {
       const newValue = value ? VARIABLE_BRACKETS_START + value.split(VARIABLE_BRACKETS_START).at(-1) : '';
       const valueToPass = getIsStringVariableReference(newValue) ? newValue : '';
 
+      inputValueRef.current = valueToPass;
       onChange(valueToPass);
       focusNextElement();
     },
     [onChange],
   );
 
-  const handleOnBlur: VariableTextProps['onBlur'] = useCallback(
-    (value: string) => {
-      if (!onBlur) {
-        return;
-      }
+  const handleOnBlur: VariableTextProps['onBlur'] = useCallback(() => {
+    if (!onBlur) {
+      return;
+    }
 
-      const newValue = value ? VARIABLE_BRACKETS_START + value.split(VARIABLE_BRACKETS_START).at(-1) : '';
-      const valueToPass = getIsStringVariableReference(newValue) ? newValue : '';
+    const inputValue = inputValueRef.current;
+    const newValue = inputValue ? VARIABLE_BRACKETS_START + inputValue.split(VARIABLE_BRACKETS_START).at(-1) : '';
+    const valueToPass = getIsStringVariableReference(newValue) ? newValue : '';
 
-      onBlur(valueToPass);
-    },
-    [onBlur],
-  );
+    onBlur(valueToPass);
+  }, [onBlur]);
 
   return (
     <div className={styles['container']}>
